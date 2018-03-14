bb714c7dcb7a42eb723b17dc9b0754e00bfcbd1d
Jordan Day <jordand@companykitchen.com>
2018-03-14 12:13:24 -0500

    Change function names. Wrong nif project lololol

--- c_src/librsync_ex_nif.h
+++ c_src/librsync_ex_nif.h
@@ -2,6 +2,7 @@
 #define LIBRSYNC_EX
 
 #include <erl_nif.h>
+#include <librsync.h>
 
 static ERL_NIF_TERM ATOM_ERROR;
 static ERL_NIF_TERM ATOM_OK;
@@ -11,11 +12,11 @@ static ERL_NIF_TERM ATOM_OK;
 
 // NIF API Functions
 // No args - return true.
-static ERL_NIF_TERM ex_pc_prox_nif_loaded(ErlNifEnv *env, int argc,
+static ERL_NIF_TERM librsync_ex_nif_loaded(ErlNifEnv *env, int argc,
                                           const ERL_NIF_TERM argv[]);
 
 // No args - return :ok on success or {:error, reason}
-static ERL_NIF_TERM ex_pc_prox_init(ErlNifEnv *env, int argc,
+static ERL_NIF_TERM librsync_ex_nif_init(ErlNifEnv *env, int argc,
                                     const ERL_NIF_TERM argv[]);
 
 
