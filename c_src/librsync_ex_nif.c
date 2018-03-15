26f60f2f57d84f4df307f9d2d4735b6d277aeb3b
Jordan Day <jordand@companykitchen.com>
2018-03-14 22:22:11 -0500

    Need to null-terminate binaries which are passed in. Not a huge fan of this impl but it was simple.

--- c_src/librsync_ex_nif.c
+++ c_src/librsync_ex_nif.c
@@ -2,6 +2,24 @@
 #include <stdio.h>
 #include <string.h>
 
+// Caller is responsible for cleanup.
+static char *get_string_from_binary(ErlNifBinary bin) {
+  // + 1 for null terminator
+  char *string = malloc(bin.size + 1);
+  memcpy(string, bin.data, bin.size);
+
+  // Add the null terminator on the end.
+  string[bin.size] = '\0';
+
+  return string;
+}
+
+static char *get_string_from_binary_term(ErlNifEnv* env, ERL_NIF_TERM bin_term) {
+  ErlNifBinary bin;
+  enif_inspect_binary(env, bin_term, &bin);
+  return get_string_from_binary(bin);
+}
+
 // NIF lifecycle functions
 static int load(ErlNifEnv *env, void **priv_data, ERL_NIF_TERM load_info) {
   ATOM_ERROR = enif_make_atom(env, "error");
@@ -22,11 +40,10 @@ static ERL_NIF_TERM librsync_ex_nif_loaded(ErlNifEnv *env, int argc, const ERL_N
 }
 
 static ERL_NIF_TERM librsync_ex_nif_rs_sig_file(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
-  ErlNifBinary old_filename, sig_filename;
-  enif_inspect_binary(env, argv[0], &old_filename);
-  enif_inspect_binary(env, argv[1], &sig_filename);
+  const char* old_filename = get_string_from_binary_term(env, argv[0]);
+  const char* sig_filename = get_string_from_binary_term(env, argv[1]);
 
-  FILE *old_file = fopen((const char*)old_filename.data, "r");
+  FILE *old_file = fopen((const char*)old_filename, "r");
   FILE *sig_file = NULL;
 
   ERL_NIF_TERM nif_result;
@@ -34,7 +51,7 @@ static ERL_NIF_TERM librsync_ex_nif_rs_sig_file(ErlNifEnv *env, int argc, const 
   if (old_file == NULL) {
     nif_result = enif_make_tuple2(env, ATOM_ERROR, enif_make_int(env, RS_IO_ERROR));
   } else {
-    sig_file = fopen((const char *)sig_filename.data, "w");
+    sig_file = fopen((const char *)sig_filename, "w");
 
     rs_result result = rs_sig_file(old_file, sig_file, RS_DEFAULT_BLOCK_LEN, RS_MAX_STRONG_SUM_LENGTH, RS_BLAKE2_SIG_MAGIC, NULL);
 
@@ -45,6 +62,9 @@ static ERL_NIF_TERM librsync_ex_nif_rs_sig_file(ErlNifEnv *env, int argc, const 
     }
   }
 
+  free((void *)old_filename);
+  free((void *)sig_filename);
+
   fclose(old_file);
   fclose(sig_file);
 
@@ -52,16 +72,12 @@ static ERL_NIF_TERM librsync_ex_nif_rs_sig_file(ErlNifEnv *env, int argc, const 
 }
 
 static ERL_NIF_TERM librsync_ex_nif_rs_delta_file(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
-  ErlNifBinary sig_filename;
-  ErlNifBinary new_filename;
-  ErlNifBinary delta_filename;
+  const char *sig_filename = get_string_from_binary_term(env, argv[0]);
+  const char *new_filename = get_string_from_binary_term(env, argv[1]);
+  const char *delta_filename = get_string_from_binary_term(env, argv[2]);
 
-  enif_inspect_binary(env, argv[0], &sig_filename);
-  enif_inspect_binary(env, argv[1], &new_filename);
-  enif_inspect_binary(env, argv[2], &delta_filename);
-
-  FILE *sig_file = fopen((const char*)sig_filename.data, "r");
-  FILE *new_file = fopen((const char*)new_filename.data, "r");
+  FILE *sig_file = fopen(sig_filename, "r");
+  FILE *new_file = fopen(new_filename, "r");
   FILE *delta_file = NULL;
 
   ERL_NIF_TERM nif_result;
@@ -79,7 +95,7 @@ static ERL_NIF_TERM librsync_ex_nif_rs_delta_file(ErlNifEnv *env, int argc, cons
       if (result != RS_DONE) {
         nif_result = enif_make_tuple2(env, ATOM_ERROR, enif_make_int(env, result));
       } else {
-        delta_file = fopen((const char*)delta_filename.data, "w");
+        delta_file = fopen(delta_filename, "w");
         result = rs_delta_file(sumset, new_file, delta_file, NULL);
 
         if (result == RS_DONE) {
@@ -91,6 +107,10 @@ static ERL_NIF_TERM librsync_ex_nif_rs_delta_file(ErlNifEnv *env, int argc, cons
     }
   }
 
+  free((void *)sig_filename);
+  free((void *)new_filename);
+  free((void *)delta_filename);
+
   fclose(sig_file);
   fclose(new_file);
   fclose(delta_file);
@@ -99,34 +119,32 @@ static ERL_NIF_TERM librsync_ex_nif_rs_delta_file(ErlNifEnv *env, int argc, cons
 }
 
 static ERL_NIF_TERM librsync_ex_nif_rs_patch_file(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
-  ErlNifBinary basis_filename;
-  ErlNifBinary delta_filename;
-  ErlNifBinary new_filename;
+  const char *basis_filename = get_string_from_binary_term(env, argv[0]);
+  const char *delta_filename = get_string_from_binary_term(env, argv[1]);
+  const char *new_filename = get_string_from_binary_term(env, argv[2]);
 
-  enif_inspect_binary(env, argv[0], &basis_filename);
-  enif_inspect_binary(env, argv[1], &delta_filename);
-  enif_inspect_binary(env, argv[2], &new_filename);
-
-  FILE *basis_file = fopen((const char*)basis_filename.data, "r");
-  FILE *delta_file = fopen((const char*)delta_filename.data, "r");
+  FILE *basis_file = fopen(basis_filename, "r");
+  FILE *delta_file = fopen(delta_filename, "r");
   FILE *new_file = NULL;
 
   ERL_NIF_TERM nif_result;
 
   if (basis_file == NULL || delta_file == NULL) {
-    printf("one of the files was NULL\n");
     nif_result = enif_make_tuple2(env, ATOM_ERROR, enif_make_int(env, RS_IO_ERROR));
   } else {
-    new_file = fopen((const char*)new_filename.data, "w");
+    new_file = fopen(new_filename, "w");
     rs_result result = rs_patch_file(basis_file, delta_file, new_file, NULL);
 
     if (result == RS_DONE) {
       nif_result = ATOM_OK;
     } else {
-      printf("error doing the patch...\n");
       nif_result = enif_make_tuple2(env, ATOM_ERROR, enif_make_int(env, result));
     }
   }
+
+  free((void *)basis_filename);
+  free((void *)delta_filename);
+  free((void *)new_filename);
 
   fclose(basis_file);
   fclose(delta_file);
